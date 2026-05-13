c9f3db49b46dd527ae78f40dcb530d6395f27c28
deathstar550 <109294121+deathstar550@users.noreply.github.com>
2026-05-12 22:45:47 -0400

    Update CSS3 color database retrieval method

--- vcf2circos/fig.py
+++ vcf2circos/fig.py
@@ -26,7 +26,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # from dash_dict import *
 def convert_rgb_to_names(rgb_tuple):
     # a dictionary of all the hex and their respective names in css3
-    css3_db = CSS3_HEX_TO_NAMES
+    css3_db = webcolors.names("css3")
     names = []
     rgb_values = []
     for color_hex, color_name in css3_db.items():
